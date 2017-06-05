91d6239be82173d18da095bfb24a944804e0884d
Mdaulet <77003820792d@gmail.com>
2017-06-05 21:25:06 +0300

    Update 2.c

--- module 5/2.c
+++ module 5/2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
-#include <math.h>
+//функция y=2*x
+double Funktion(double x)
+{
+    return 2*x;
+}
 double Simpson(double a,double b,int n)
 {
     int  i = 1;
@@ -12,15 +16,15 @@ double Simpson(double a,double b,int n)
             c=c+h;
             if(i%2!=0)
                 {
-                    sum=sum+2*c;
+                    sum=sum+Funktion(c);
                 }
             else
                 {
-                    sum_1=sum_1+2*c;
+                    sum_1=sum_1+Funktion(c);
                 }
                 i++;
         }
-        integral=h/3*(2*a+2*b+4*sum+2*sum_1);
+        integral=h/3*(Funktion(a)+Funktion(b)+4*sum+2*sum_1);
     return integral;
 }
 int main(void)
@@ -28,11 +32,11 @@ int main(void)
     double dia1 , dia2, x1 ;
     int x;
     setlocale(LC_ALL,"Rus");
-    printf("Введи начало диапазона(для функции y=2x) ");
+    printf("Введите начало диапазона(для функции y=2x) ");
     scanf("%lf",&dia1);
-    printf("Введи конец диапазона ");
+    printf("Введите конец диапазона ");
     scanf("%lf",&dia2);
-    printf("Введи  количество разбиений: ");
+    printf("Введите  количество разбиений: ");
     scanf("%i",&x);
     printf("Ответ: %lf\n",Simpson(dia1,dia2,x));
     return 0;
