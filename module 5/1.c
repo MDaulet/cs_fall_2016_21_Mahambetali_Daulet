1546c646222962459be51aba954f46348aec81f1
Mdaulet <77003820792d@gmail.com>
2017-06-05 21:23:57 +0300

    Update 1.c

--- module 5/1.c
+++ module 5/1.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+float Funktion(float x)
+{
+    return x*x;
+}
 void Diff(float x1,float x2,float h)
 {
     float dif3 = 0,xi=0;
     int i;
-    printf("%f\n",1/(2*h)*(-3*x1*x1+4*pow(x1+h,2)-pow(x1+2*h,2)));
-    printf("%f\n",1/(2*h)*(pow(x1+2*h,2)-x1*x1));
+    printf("%f\n",1/(2*h)*(-3*Funktion(x1)+4*Funktion(x1+h)-Funktion(x1+2*h)));
+    printf("%f\n",1/(2*h)*(Funktion(x1+2*h)-Funktion(x1)));
     xi=x1;
-    while(xi<=x2)
+    while(xi+2*h<=x2)
         {
-            printf("%f\n",1/(2*h)*(xi*xi-4*pow(xi+h,2)+3*pow(xi+2*h,2)));
+            printf("%f\n",1/(2*h)*(Funktion(xi)-4*Funktion(xi+h)+3*Funktion(xi+2*h)));
             xi=xi+h;
         }
 }
@@ -15,11 +21,11 @@ int main(void)
 {
     double dia1 , dia2, x1 ;
     setlocale(LC_ALL,"Rus");
-    printf("Введи начало диапазона(функция y=x^2)");
+    printf("Введите начало диапазона(функция y=x^2)");
     scanf("%lf",&dia1);
-    printf("Введи конец диапазона ");
+    printf("Введите конец диапазона ");
     scanf("%lf",&dia2);
-    printf("Введи шаг ");
+    printf("Введите шаг ");
     scanf("%lf",&x1);
     Diff(dia1,dia2,x1);
     return 0;
